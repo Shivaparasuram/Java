dc1bace98ba69544700be4cbd61b21f61cf94a2e
Shiva Parasuram R <shivaparasuramr@gmail.com>
2026-01-13 10:03:52 +0530

    Updated

--- src/Pattern_3/MIRROR_REV.java
+++ src/Pattern_3/MIRROR_REV.java
@@ -3,15 +3,18 @@ package Pattern_3;
 public class MIRROR_REV
 {
     static void main() {
-        int n = 4 ;
-        for (int i = 1; i <= n; i++)
-        {
+        int n = 5;
+        for (int i = n; i >= 1; i--) {
+
             for (int j = n; j > i; j--) {
                 System.out.print(" ");
             }
             for (int k = 1; k <= i; k++) {
 
-                System.out.print("*");
+
+                    System.out.print("*");
+
+
             }
             System.out.println(" ");
         }
