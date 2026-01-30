53b7dce1a034743960957a3125c287a17f3c593f
Shiva Parasuram R <shivaparasuramr@gmail.com>
2026-01-30 12:27:49 +0530

    Commit

--- src/in.java
+++ src/in.java
@@ -1,8 +1,8 @@
 public class in {
     static void main() {
-        int n=4;
-        for (int i=n;i>1;i--){
-            for (int j=n;j<=1;j++)
+        int n = 5;
+        for (int i=1; i<=5;i++){
+            for (int j=1;j<=i;j++)
             {
                 System.out.print("*");
             }
